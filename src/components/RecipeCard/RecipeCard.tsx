c5b47ebd523299947a56a645412d6c4222e07930
Gabriel <gmamanican@unsa.edu.pe>
2023-11-11 17:21:09 -0500

    modificando componente para que reciba parametros

--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -4,32 +4,46 @@ import VisibilityIcon from "@mui/icons-material/Visibility";
 import { RatingStars } from "../Rating/RatingStars";
 import "./_RecipeCard.scss";
 
-export const RecipeCard = () => {
+interface RecipeCardProps {
+  title: string;
+  user_name: string;
+  images: string[];
+  views: number;
+  average_rating: number;
+}
+
+export const RecipeCard = ({
+  title,
+  user_name,
+  views,
+  average_rating,
+  images,
+}: RecipeCardProps) => {
+  const imageURL = images[Math.floor(Math.random() * images.length)];
+
   return (
     <Grid container spacing={2} className="recipeCard__container">
       <Grid item className="recipeCard__container-imageContainer">
-        <img
-          alt="NameImage"
-          src="https://www.recetasnestlecam.com/sites/default/files/styles/crop_article_banner_desktop_nes/public/2023-02/postres-con-mango.jpg.jpg?itok=c6AgNlbK"
-        />
+        <img alt={title} src={imageURL} />
       </Grid>
       <Grid item xs={12} sm container>
         <Grid item xs container direction="column" spacing={1}>
           <Grid item xs className="recipeCard__container-info">
-            <Typography gutterBottom variant="subtitle1" component="div">
-              Nombre elemento
+            <Typography gutterBottom variant="h6" component="div">
+              {title}
             </Typography>
             <Typography variant="body2" gutterBottom>
-              Publicador
+              {user_name}
             </Typography>
           </Grid>
           <Grid item className="recipeCard__container-footer">
             <Typography variant="body2">
-              <span>10</span>
+              <span>
+                <b>{views}</b>
+              </span>
               <VisibilityIcon />
             </Typography>
-            {/* <Typography variant="body2">5 Estrellas</Typography> */}
-            <RatingStars readOnly={true} qualification={2} />
+            <RatingStars readOnly={true} qualification={average_rating} />
           </Grid>
         </Grid>
       </Grid>
