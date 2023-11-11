5467c620bb54dc0b4e37aa1db7590269bca224b6
Gabriel <gmamanican@unsa.edu.pe>
2023-11-11 17:21:28 -0500

    llamando componente con parametros

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,12 +1,15 @@
-import { ButtonBase, Container, Grid, Paper, Typography } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 import "./_Categories.scss";
 import { RecipeCard } from "../../components/RecipeCard/RecipeCard";
+import { NavBar } from "../../components/NavBar/NavBar";
+import { SearchBar } from "../../components/SearchBar/SearchBar";
+import { recetas } from "../../static_test/recipes";
 
 export const Categories = () => {
   return (
     <>
-      {/* Navbar */}
-      <Container maxWidth="sm" className="category__container">
+      <NavBar />
+      <div className="category__container">
         <Typography
           variant="h4"
           component="h1"
@@ -14,7 +17,9 @@ export const Categories = () => {
         >
           <b>Categoría</b>
         </Typography>
-        <p>search component</p>
+        <div className="category__container-searchBar">
+          <SearchBar />
+        </div>
         <Paper
           sx={{
             p: 5,
@@ -24,12 +29,20 @@ export const Categories = () => {
             backgroundColor: (theme) =>
               theme.palette.mode === "dark" ? "#1A2027" : "#fff",
           }}
+          className="category__container-paper"
         >
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
+          {recetas.map(({ average_rating, title, images, views }, index) => (
+            <RecipeCard
+              average_rating={average_rating}
+              title={title}
+              images={images}
+              views={views}
+              user_name="Temporal"
+              key={index}
+            />
+          ))}
         </Paper>
-      </Container>
+      </div>
     </>
   );
 };
