e9b5e5e8ca14a0fa8655dce991ca7ec95062eadd
Edson Lopez Quispe <elopezqu@unsa.edu.pe>
2023-11-12 17:12:17 -0500

    Añadiendo los elementos de la pagina de Registro

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,38 +1,83 @@
 import { useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
 import "./_Register.scss";
 
-interface credentials  {
-    email: string;
-    password: string,
+interface dataUser  {
+    name: String,
+    lastName: String,
+    email: String,
+    password: String,
+    verifyPassword: String,
+    rol: String
 };
+
 export const Register = () =>{
-    const [form, setForm] = useState<credentials>({
+    const [form, setForm] = useState< dataUser >({
+        name: '',
+        lastName: '',
         email: '',
         password: '',
-      });
-    
-      const inputChange = (value: string, field: string) => {
+        verifyPassword: '',
+        rol: ''
+    });
+
+    const inputChange = (value: string, field: string) => {
         setForm({
           ...form,
           [field]: value,
         });
-      };
-      console.log(form);
+    }
+    console.log(form);
+
     return (
-        <>
-        <div className="container">
-          <div className="loginTitle">
-            <h1 className="loginTitle__t">
-            ¡Bienvenid@ a Compartiendo
+      <>
+        <div className="registerContainer">
+          <div className="registerTitle">
+            <h1 className="registerTitle__t">
+                Registrarse
             <br />
-            Sabores!
             </h1>
           </div>
-          <form className="loginForm">
-            <div className="loginForm__email">
-              <div className="loginForm__email-label">
+          <form className="registerForm">
+            <div className="registerForm__name">
+              <div className="registerForm__name-label">
+                <label >
+                Nombres:
+                </label>
+              </div>
+              <TextField
+                required
+                id="name"
+                className="registerForm__name-input"
+                value={form.name}
+                onChange={({ target }) => {
+                  inputChange(target.value, "name");
+                }}
+              />
+            </div>
+            <div className="registerForm__lastName">
+              <div className="registerForm__lastName-label">
+                <label >
+                Apellidos:
+                </label>
+              </div>
+              <TextField
+                required
+                id="lastName"
+                className="registerForm__lastName-input"
+                value={form.lastName}
+                onChange={({ target }) => {
+                  inputChange(target.value, "lastName");
+                }}
+              />
+            </div>
+            <div className="registerForm__email">
+              <div className="registerForm__email-label">
                 <label >
                 Correo Electronico:
                 </label>
@@ -41,15 +86,15 @@ export const Register = () =>{
                 required
                 id="email"
                 type="email"
-                className="loginForm__email-input"
+                className="registerForm__email-input"
                 value={form.email}
                 onChange={({ target }) => {
                   inputChange(target.value, "email");
                 }}
               />
             </div>
-            <div className="loginForm__password">
-              <div className="loginForm__email-label">
+            <div className="registerForm__password">
+              <div className="registerForm__password-label">
                 <label>
                   Contraseña:
                 </label>
@@ -59,30 +104,70 @@ export const Register = () =>{
                 value={form.password}
                 id="password"
                 type="password"
-                className="loginForm__password-input"
+                className="registerForm__password-input"
                 onChange={({ target }) => {
                   inputChange(target.value, "password");
                 }}
               />
             </div>
-            <div className="loginForm__btn-login">
-              <Button
-                variant="contained"
-                className="loginForm__btn-login-b"
-              >
-                Iniciar Sesión
-              </Button>
+            <div className="registerForm__verifyPassword">
+              <div className="registerForm__verifyPassword-label">
+                <label>
+                  Confirmar contraseña:
+                </label>
+              </div>
+              <TextField
+                required
+                value={form.verifyPassword}
+                id="verifyPassword"
+                type="password"
+                className="registerForm__verifyPassword-input"
+                onChange={({ target }) => {
+                  inputChange(target.value, "verifyPassword");
+                }}
+              />
             </div>
-            <div className="loginForm__btn-register">
+            <div className="registerForm__userType">
+              <label>Tipo de uso:</label>
+              <br />
+              <FormControl className="registerForm__userType-formControl">
+                <RadioGroup
+                    row
+                    name="userType"
+                    defaultValue={"Personal"}
+                    className="registerForm__userType-formControl-radioGroup"
+                >   
+                    
+                    <FormControlLabel
+                    value="Personal"
+                    control={<Radio />}
+                    label="Personal"
+                    onClick={({}) => {
+                        inputChange("Personal", "rol");
+                    }}
+                    />
+
+                    <FormControlLabel
+                    value="Empresa"
+                    control={<Radio />}
+                    label="Empresa"
+                    onClick={({}) => {
+                        inputChange("Empresa", "rol");
+                    }}
+                    />
+                </RadioGroup>
+              </FormControl>
+            </div>
+            <div className="registerForm__btn-create">
               <Button
                 variant="contained"
-                className="loginForm__btn-register-b"
+                className="registerForm__btn-create-b"
               >
-                Registrase
+                Crear Cuenta
               </Button>
             </div>
           </form>
         </div>
-        </>
-      );
+      </>
+    );
 }
