f78c9c21781856d5ae6cbfee52b80a0a2b434abf
Edson Lopez Quispe <elopezqu@unsa.edu.pe>
2023-11-17 11:13:44 -0500

    Corrigiendo conflictos

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { StyledEngineProvider } from "@mui/material";
-<<<<<<< HEAD
 
 import App from "./App";
-=======
-import { Categories } from "./pages/Categories/Categories";
-import { Register } from "./pages/Register/Register";
-import { CreateRecipe } from "./pages/CreateRecipe/CreateRecipe";
->>>>>>> 9d39982667ef893364e7ac0bc492c4d51f0b7af1
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -21,11 +15,7 @@ root.render(
       <App />
       {/*<Login />*/}
 
-<<<<<<< HEAD
       {/* <Categories /> */}
-=======
-      <CreateRecipe />
->>>>>>> 9d39982667ef893364e7ac0bc492c4d51f0b7af1
 
       {/* <HomePage /> */}
 
