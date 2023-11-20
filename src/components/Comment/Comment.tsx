9d4a78d31e4950b838838f8dfb7e2dd939aadc44
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2023-11-19 21:50:52 -0500

    añadiendo componente Comment

---
+++ src/components/Comment/Comment.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { RatingStars } from "../Rating/RatingStars";
+import "./_Comment.scss";
+
+interface CommentProps {
+  author: string;
+  comment: string;
+  rating: number;
+}
+
+export const Comment = ({ author, comment, rating }: CommentProps) => {
+  return (
+    <div className="comment">
+      <div className="comment__header">
+        <h4 className="comment__header-author">{author}</h4>
+        <RatingStars readOnly qualification={rating} />
+      </div>
+      <p className="comment__commentContent">{comment}</p>
+    </div>
+  );
+};
---
+++ src/static_test/comments.js
@@ -0,0 +1,28 @@
+export const comments = [
+  {
+    id: 1,
+    author: "foodlover22",
+    comment:
+      "¡Este plato es una delicia! La combinación de sabores es increíble.",
+    rating: 4,
+  },
+  {
+    id: 2,
+    author: "chefmaster99",
+    comment: "La presentación es impecable, ¡y el sabor no se queda atrás!",
+    rating: 5,
+  },
+  {
+    id: 3,
+    author: "healthyeats4life",
+    comment: "Me encanta que sea tan saludable y delicioso al mismo tiempo.",
+    rating: 3,
+  },
+  {
+    id: 4,
+    author: "foodieadventurer",
+    comment:
+      "Probé este plato en un viaje y ahora lo busco en todos lados, ¡es mi favorito!",
+    rating: 5,
+  },
+];
