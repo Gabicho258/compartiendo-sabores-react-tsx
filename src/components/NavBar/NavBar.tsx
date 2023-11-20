7cb9a7f50df524babc83a448dbf1570681837daf
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2023-11-20 17:45:09 -0500

    añadiendo menu

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,29 @@
 import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Divider from "@mui/material/Divider";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
 import "./_NavBar.scss";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { users } from "../../static_test/users";
 
-const isUserAuthenticated = false;
+const isUserAuthenticated = true;
+const user = users[0];
 
 export const NavBar = () => {
   const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
       <div className="navBarContainer">
@@ -18,12 +35,68 @@ export const NavBar = () => {
         {isUserAuthenticated ? (
           <>
             <div className="navBarContainer__profile">
-              <div className="navBarContainer__profile-name">Juan Perez</div>
+              <div className="navBarContainer__profile-name">
+                {`${user.first_name} ${user.last_name}`}
+              </div>
               <Avatar
                 className="navBarContainer__profile-image"
-                alt="Juan Perez"
-                src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                alt={user.first_name}
+                src={user.photo_url}
+                onClick={handleClick}
               />
+              <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                  elevation: 0,
+                  sx: {
+                    overflow: "visible",
+                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                    mt: 1.5,
+                    "& .MuiAvatar-root": {
+                      width: 32,
+                      height: 32,
+                      ml: -0.5,
+                      mr: 1,
+                    },
+                    "&:before": {
+                      content: '""',
+                      display: "block",
+                      position: "absolute",
+                      top: 0,
+                      right: 14,
+                      width: 10,
+                      height: 10,
+                      bgcolor: "background.paper",
+                      transform: "translateY(-50%) rotate(45deg)",
+                      zIndex: 0,
+                    },
+                  },
+                }}
+                transformOrigin={{ horizontal: "right", vertical: "top" }}
+                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+              >
+                <MenuItem onClick={handleClose}>
+                  <Avatar src={user.photo_url} /> {user.first_name}
+                </MenuItem>
+
+                <Divider />
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <Settings fontSize="small" />
+                  </ListItemIcon>
+                  Editar perfil
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                  <ListItemIcon>
+                    <Logout fontSize="small" />
+                  </ListItemIcon>
+                  Salir
+                </MenuItem>
+              </Menu>
             </div>
           </>
         ) : (
