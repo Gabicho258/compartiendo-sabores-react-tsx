e3f31e7c18311be37ca1d55614ca725385cc8d5a
Gabriel <gmamanican@unsa.edu.pe>
2023-11-27 18:41:58 -0500

    navegación a usuario desde comentario

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { RatingStars } from "../Rating/RatingStars";
 import "./_Comment.scss";
+import { useNavigate } from "react-router-dom";
 
 interface CommentProps {
   author: string;
   comment: string;
   rating: number;
+  user_id: string;
 }
 
-export const Comment = ({ author, comment, rating }: CommentProps) => {
+export const Comment = ({ author, comment, rating, user_id }: CommentProps) => {
+  const navigate = useNavigate();
   return (
     <div className="comment">
       <div className="comment__header">
-        <h4 className="comment__header-author">{author}</h4>
+        <h4
+          className="comment__header-author"
+          onClick={() => {
+            navigate(`/profile/${user_id}`);
+          }}
+        >
+          {author}
+        </h4>
         <RatingStars readOnly darkTheme qualification={rating} />
       </div>
       <p className="comment__commentContent">{comment}</p>
