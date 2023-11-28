1d73dce575948ba111c721fdad4a76ba441d80ac
Gabriel <gmamanican@unsa.edu.pe>
2023-11-28 18:16:59 -0500

    conectando visión de chats y envio de mensajes

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react";
 import { NavBar } from "../../components/NavBar/NavBar";
 //import { useNavigate } from "react-router-dom";
-import { users } from "../../static_test/users";
+// import { users } from "../../static_test/users";
 import { messages } from "../../static_test/message";
 import "./_Chat.scss";
+import {
+  useCreateMessageMutation,
+  useGetChatsByUserIdQuery,
+  useGetUsersQuery,
+} from "../../app/apis/compartiendoSabores.api";
+import { User, Chat as iChat } from "../../interfaces";
 
-interface Friend{
-  name:string;
-  image:string;
+interface Friend {
+  name: string;
+  image: string;
 }
 interface Message {
   isFriend: boolean;
@@ -17,34 +23,61 @@ interface Message {
   __v: number;
 }
 
-
 export const Chat = () => {
+  const isUserAuthenticated = localStorage.getItem("data");
+  const userCredentials =
+    isUserAuthenticated && JSON.parse(isUserAuthenticated);
+  const { data: myChats = [] } = useGetChatsByUserIdQuery(userCredentials.id);
+  const { data: users = [] } = useGetUsersQuery();
+  const [createMessage] = useCreateMessageMutation();
+  const sortData = <T extends Message | iChat>(data: T[]): T[] => {
+    const sortedData = [...data].sort((a, b) => {
+      const fechaA = new Date(a.updatedAt).getTime();
+      const fechaB = new Date(b.updatedAt).getTime();
+      return fechaA - fechaB;
+    });
+    if (data.length > 0 && "members" in data[0]) {
+      return sortedData.reverse() as T[];
+    }
+
+    return sortedData;
+  };
+  ////////////
   const [form, setForm] = useState<Partial<Message>>({
-    text:'',
-  });
-  const [sortMessages, setSortMessages] = useState<Message[]>([]);
-  const [friend, setFriend] = useState<Friend>({
-    name:'',
-    image:'',
+    text: "",
   });
 
-  const inputForm = (t: string)=>{
+  const [sortMessages, setSortMessages] = useState<Message[]>([]);
+  const [friendSelected, setFriendSelected] = useState<User>();
+  const [chatSelected, setChatSelected] = useState<iChat>();
+
+  const inputForm = (t: string) => {
     setForm({
-      text:t,
-    })
-  }
-  const print = ()=>{
-    console.log(form);
-    setForm({
-      text:'',
-    })
-  }
-  const openChat = (n: string, img: string)=> {
-    setFriend({
-      name:n,
-      image:img,
+      text: t,
     });
-  }
+  };
+  const handleSendMessage = async () => {
+    const message = {
+      ...form,
+      chat_id: chatSelected?._id,
+      sender_id: userCredentials.id,
+    };
+    try {
+      const response = await createMessage(message).unwrap();
+      console.log(response);
+      console.log(message);
+    } catch (error: any) {
+      alert(JSON.stringify(error.data));
+    }
+    setForm({
+      text: "",
+    });
+  };
+  const handleOpenChat = (friend: User, chat: iChat) => {
+    setFriendSelected(friend);
+    setChatSelected(chat);
+  };
+
   useEffect(() => {
     const sortMessages = [...messages].sort((a, b) => {
       const fechaA = new Date(a.createdAt).getTime();
@@ -54,7 +87,7 @@ export const Chat = () => {
     setSortMessages(sortMessages);
   }, []);
 
-  const newDate = (data: string) =>{
+  const newDate = (data: string) => {
     const dateMongoDB = new Date(data);
     const year = dateMongoDB.getFullYear();
     const month = dateMongoDB.getMonth() + 1; // Meses en JavaScript son 0-indexados
@@ -68,69 +101,106 @@ export const Chat = () => {
       day,
       hour,
       minute,
-      second
+      second,
     };
-  }
+  };
 
   return (
     <>
-        <NavBar />
-        <div className="chat__container">
-          <div className="chat__friends">
-            <h2>CHATS</h2>
-            <div className="chat__friends-names">
-              {users.map((user, index) => 
-              <div key={index} className="chat__friends-names-n" 
-                onClick={()=>{openChat(`${user.first_name} ${user.last_name}`,`${user.photo_url}` )}}
-                ><img className="chat__friends-names-n-i" src={user.photo_url}></img><p>{user.first_name} {user.last_name}</p>
-                </div>)}
-            </div>
-          </div>
-          <div className="chat__message">
-            <div className="chat__message-friend">
-              <img  className="chat__message-friend-i" src={friend.image}></img><h2>{friend.name}</h2> 
-            </div>
-            <div className="chat__message-chat" >
-                <div className="chat__message-chat-c" >
-                  {sortMessages.map((message, index) => (
-                    <div key={index}>
-                      {message.isFriend ? (
-                      <div className="chat__message-chat-c-m1" >
-                        <div className="chat__message-chat-c-m1-p">
-                          <p>{message.text}</p> <label>{newDate(message.createdAt).hour}:{newDate(message.createdAt).minute}</label>
-                        </div>
-                      </div>
-                      ) : (
-                      <div className="chat__message-chat-c-m2" >
-                        <div className="chat__message-chat-c-m2-p">
-                          <p>{message.text}</p><label>{newDate(message.createdAt).hour}:{newDate(message.createdAt).minute}</label>
-                        </div>
-                      </div>
-                      )}
-                    </div>
-                  ))}
-                </div>
-                <div className="chat__message-chat-b">
-                  <input 
-                    value={form.text}
-                    className="chat__message-chat-b-in" 
-                    onChange={({ target }) => {
-                      inputForm(target.value);
-                    }}
-                  ></input> 
-                  <img 
-                    className="chat__message-chat-b-i" 
-                    src="https://cdn.pixabay.com/photo/2016/07/12/21/00/paper-planes-1513032_1280.png" 
-                    onClick={({})=>{
-                      print();
-                    }}
+      <NavBar />
+      <div className="chat__container">
+        <div className="chat__friends">
+          <h2>CHATS</h2>
+          <div className="chat__friends-names">
+            {sortData(myChats).map((chat, index) => {
+              const [friendUser] = users?.filter(
+                (user) =>
+                  user._id ===
+                  chat.members.find(
+                    (memberId) => memberId !== userCredentials.id
+                  )
+              );
+
+              return (
+                <div
+                  key={index}
+                  className="chat__friends-names-n"
+                  onClick={() => {
+                    handleOpenChat(friendUser, chat);
+                  }}
+                >
+                  <img
+                    alt={`${friendUser?.first_name} ${friendUser?.last_name}`}
+                    className="chat__friends-names-n-i"
+                    src={friendUser?.photo_url}
                   ></img>
+                  <p>
+                    {friendUser?.first_name} {friendUser?.last_name}
+                  </p>
                 </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="chat__message">
+          <div className="chat__message-friend">
+            <img
+              alt="qweqwe"
+              className="chat__message-friend-i"
+              src={friendSelected?.photo_url}
+            ></img>
+            <h2>
+              {friendSelected?.first_name} {friendSelected?.last_name}
+            </h2>
+          </div>
+          <div className="chat__message-chat">
+            <div className="chat__message-chat-c">
+              {sortMessages.map((message, index) => (
+                <div key={index}>
+                  {message.isFriend ? (
+                    <div className="chat__message-chat-c-m1">
+                      <div className="chat__message-chat-c-m1-p">
+                        <p>{message.text}</p>{" "}
+                        <label>
+                          {newDate(message.createdAt).hour}:
+                          {newDate(message.createdAt).minute}
+                        </label>
+                      </div>
+                    </div>
+                  ) : (
+                    <div className="chat__message-chat-c-m2">
+                      <div className="chat__message-chat-c-m2-p">
+                        <p>{message.text}</p>
+                        <label>
+                          {newDate(message.createdAt).hour}:
+                          {newDate(message.createdAt).minute}
+                        </label>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+            <div className="chat__message-chat-b">
+              <input
+                value={form.text}
+                className="chat__message-chat-b-in"
+                onChange={({ target }) => {
+                  inputForm(target.value);
+                }}
+              />
+              <img
+                alt="Send button"
+                className="chat__message-chat-b-i"
+                src="https://cdn.pixabay.com/photo/2016/07/12/21/00/paper-planes-1513032_1280.png"
+                onClick={() => {
+                  handleSendMessage();
+                }}
+              />
             </div>
           </div>
-            
         </div>
-        
+      </div>
     </>
   );
 };
